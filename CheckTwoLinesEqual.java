bd8f4a08a091add7fabf776e1d5b9514ef597dd3
Nikita-sah <nikitasah4gmail.com>
2022-09-01 17:33:06 +0530

    changes

--- CheckTwoLinesEqual.java
+++ CheckTwoLinesEqual.java
@@ -1,39 +1,42 @@
 package com.lineComparisonProblem;
 
 public class CheckTwoLinesEqual {
+	
+	int x1 = 2;
+	int x2 = 4;
+	int y1 = 2;
+	int y2 = 4;
+	
+	int a1 = 2;
+	int a2 = 4;
+	int b1 = 2;
+	int b2 = 4;
+
+	String z1 ="0";
+	String z2 ="0";
 
 	public static void main(String[] args) {
-		int x1 = 2;
-		int x2 = 4;
-		int y1 = 2;
-		int y2 = 4;
-		
-		int a1 = 2;
-		int a2 = 4;
-		int b1 = 2;
-		int b2 = 4;
+		 
+		CheckTwoLinesEqual TwoLinesEqual = new CheckTwoLinesEqual();
 	
-		String z1 ="0";
-		String z2 ="0";
-				
-		float Line1_Length =  (float) Math.sqrt( Math.pow (x2 - x1 ,2) + Math.pow (y2- y1 , 2));
+		float Line1_Length =  (float) Math.sqrt( Math.pow (TwoLinesEqual.x2 - TwoLinesEqual.x1 ,2) + Math.pow (TwoLinesEqual.y2- TwoLinesEqual.y1 , 2));
 			
-		z1 =String.valueOf(Line1_Length);  
+		TwoLinesEqual.z1 =String.valueOf(Line1_Length);  
 		
 		System.out.println( "Line1_Length" );
 		System.out.println( Line1_Length );
 		
-		float Line2_Length =  (float) Math.sqrt( Math.pow (a2 - a1 ,2) + Math.pow (b2- b1 , 2));
+		float Line2_Length =  (float) Math.sqrt( Math.pow (TwoLinesEqual.a2 - TwoLinesEqual.a1 ,2) + Math.pow (TwoLinesEqual.b2- TwoLinesEqual.b1 , 2));
 		
-		z2 =String.valueOf(Line2_Length);
+		TwoLinesEqual.z2 =String.valueOf(Line2_Length);
 		
 		System.out.println( "Line2_Length");
 		System.out.println( Line2_Length );
 		
-		if(z1.equals(z2))
+		if(TwoLinesEqual.z1.equals(TwoLinesEqual.z2))
 				
 		{
-			System.out.println(z1.equals(z2));			
+			System.out.println(TwoLinesEqual.z1.equals(TwoLinesEqual.z2));			
 			System.out.println("Both Line equal");	
 
 	      }
